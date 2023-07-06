87a308ee18017007c7667aab3ee3296ce8f710fd
선민 박 <park.sm@tdgl.co.kr>
2023-07-06 16:36:12 +0900

    go

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 // Resource Group-테라폼 DevOps Test
 variable "resource-group-name" {   
     type = string
-    default = "rg-krc-devops-hklee1"
+    default = "PSM-ASP-TEST"
 }
 
 variable "resource-group-location" {   
