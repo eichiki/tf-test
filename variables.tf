702f86609ce9ab8b0204b4288ce09917c8ad8ebd
eichiki <85059332+eichiki@users.noreply.github.com>
2023-07-13 14:12:25 +0900

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,9 +1,24 @@
-// Resource Group-테라폼 DevOps Test
-variable "resource-group-name" {   
-    type = string
-    default = "PSM-ASP-TEST"
+variable "resource_group_location" {
+  type        = string
+  description = "Location for all resources."
+  default     = "eastus"
 }
 
-variable "resource-group-location" {   
-    default = "koreacentral"
+variable "resource_group_name_prefix" {
+  type        = string
+  description = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+  default     = "rg"
+}
+
+variable "user_name" {
+  type        = string
+  description = "The username for the local account that will be created on the new vm."
+  default     = "azureadmin"
+}
+
+variable "password" {
+  type        = string
+  description = "The password for the local account that will be created on the new vm."
+  sensitive   = true
+  default     = null
 }
